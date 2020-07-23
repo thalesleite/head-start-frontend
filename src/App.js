d939e6e06af43cf25823ca11ed9f9fb6a6ff69d9
thalesleite <thalesaleite@gmail.com>
2020-07-23 09:25:23 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 import HomePage from './pages/Home/Home';
 import AboutUsPage from './pages/AboutUs/AboutUs';
@@ -17,15 +18,16 @@ function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/about-us' component={AboutUsPage} />
-        <Route path='/courses' component={CoursesPage} />
-        <Route path='/contact' component={ContactPage} />
-        <Route path='/login' component={LoginPage} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/about-us' component={AboutUsPage} />
+          <Route path='/courses' component={CoursesPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/login' component={LoginPage} />
 
-        <Route path='/dashboard' component={DashboardPage} />
-      </Switch>
+          <Route path='/dashboard' component={DashboardPage} />
+        </Switch>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Typography>
+        <p>Copyright © 2020 Head Start</p>
+      </Typography>
+    </footer>
+  );
+}
+
+export default Footer;
