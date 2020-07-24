c8588edd61d3fd0355fe2741d50ef5facec3baea
thalesleite <thalesaleite@gmail.com>
2020-07-24 23:57:35 +0100

    styled base services page

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,13 +1,106 @@
 import React from 'react';
+
+import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+
 import './Services.scss';
 
 function Services() {
   return (
     <div 
       id="services" 
-      className="container"
+      className="container txt-center services"
     >
-      
+      <h1>SERVICES</h1>
+
+      <h2>Online Courses</h2>
+      <section className="services-cards">
+        <Card className="services-card">
+          <CardContent>
+            <h3>Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
+              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
+              nisi consectetur impedit, ut ullam nesciunt.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </section>
+
+      <h2>Face-to-face Courses</h2>
+      <section className="services-cards">
+        <Card className="services-card">
+          <CardContent>
+            <h3>Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
+              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
+              nisi consectetur impedit, ut ullam nesciunt.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card className="services-card">
+          <CardContent>
+            <h3>Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
+              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
+              nisi consectetur impedit, ut ullam nesciunt.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card className="services-card">
+          <CardContent>
+            <h3>Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
+              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
+              nisi consectetur impedit, ut ullam nesciunt.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card className="services-card">
+          <CardContent>
+            <h3>Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
+              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
+              nisi consectetur impedit, ut ullam nesciunt.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+        <Card className="services-card">
+          <CardContent>
+            <h3>Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
+              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
+              nisi consectetur impedit, ut ullam nesciunt.
+            </p>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </section>
     </div>
   );
 }
