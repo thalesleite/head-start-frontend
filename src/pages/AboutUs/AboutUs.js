05d34b7313cf4ef6c3f39951dc4b0f262202dd1b
thalesleite <thalesaleite@gmail.com>
2020-07-30 12:19:01 +0100

    created layout for cards in services page

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -13,35 +13,79 @@ function AboutUs() {
     >
 
       <section className="about-section">
-        <h1>HEAD START</h1>
+        <h1>ABOUT US</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, 
-          eos repudiandae nulla libero porro eius, maxime, pariatur quas omnis similique error 
-          impedit rerum! Illum sequi possimus cupiditate obcaecati, nobis repellendus.
+          Located in the heart of Dublin, the Head Start Courses is formed by Bartira Augelli, 
+          an experienced and qualified professional focused on providing accessible services for the food sector, 
+          including “Food Safety (HACCP - Level 1)” and “Barista” courses.
+          Our courses are available face to face or online, either English or Portuguese language, helping students, 
+          especially those immigrants and who need effective and clear content to get a job in Ireland.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, 
-          eos repudiandae nulla libero porro eius, maxime, pariatur quas omnis similique error 
-          impedit rerum! Illum sequi possimus cupiditate obcaecati, nobis repellendus.
+          With a simple, engaging and dynamic language, the Head Start Courses gives support by adequate 
+          training, and also helps the students with their Curriculum Vitae, with the objective to develop 
+          the necessary tools and confidence in order to distinguish our students toward the labour market, 
+          contributing to their personal and professional journey.
+        </p>
+
+        <b>MISSION</b>
+        <p>
+          Delivering knowledgment in a light and efficient way, growing personally and professionally the student. 
+        </p>
+
+        <b>VISION</b>
+        <p>
+          To become a benchmark in forming outstanding professionals of the Irish food sector, 
+          contributing with the immigrant’s lives and also with the country. 
+        </p>
+
+        <b>VALUES</b>
+        <p>
+          Honesty, Integrity, Respect and Inclusion.
         </p>
       </section>
 
       <section className="about-section">
-        <h1>BARTIRA</h1>
+        <h1>WHO IS BARTIRA?</h1>
         <div className="about-founder">
         
           <Parallax y={[-20, 20]}>
             <img className="bartira-img" src={BartiraImg} alt="Bartira"/>
+            <p>
+              <i>
+                “My business started when I realized that my tools and experience could create a 
+                positive impact on people’s lives, especially immigrants like me. Since then, my 
+                happiness is doing what I love, knowing that I can contribute to the start of so 
+                many professionals.”
+              </i> (BARTIRA)
+            </p>
           </Parallax>
           
           <div className="description">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, 
-              eos repudiandae nulla libero porro eius, maxime, pariatur quas omnis similique error 
-              impedit rerum! Illum sequi possimus cupiditate obcaecati, nobis repellendus.
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, 
-              eos repudiandae nulla libero porro eius, maxime, pariatur quas omnis similique error 
-              impedit rerum! Illum sequi possimus cupiditate obcaecati, nobis repellendus.
+              With almost 10 years’ experience in irish food sector, Bartira Augelli is a brazilian entrepreneur, 
+              teacher and Master in "MSc in Entrepreneurship" by Trinity College Dublin (TCD), 
+              studying the necessity of food safety training in Ireland.
+            </p>
+            <p>
+              The professional also has training with certification in “Advanced HACCP Design and Validation based on 
+              Risk Assessment (Level 6)" by QQI; Master by International College Dublin (ICD)  - 
+              "BA (Honours) Business Studies" - in Business and Training; and 
+              Certification by School of Education - Trinity College Dublin (TCD) in “Mentoring Training” .
+            </p>
+            <p>
+              Bartira is the founder of Head Start Courses and of the renowned “Barista Course By Bartira”, 
+              that in 7 years already has qualified more than 5000 professionals. She also is the owner of 
+              the stylish coffee shop located in British Studies Centres, Dublin.
+            </p>
+            <p>
+              She spends part of her time giving talks, training and workshops, as well working in social programs, 
+              teaching for a challenging group of adolescents that belong to underserved areas around Dublin. 
+            </p>
+            <p>
+              Bartira is a member and works as tutor for “Career LEAP” Work Readiness Training Programme, 
+              a program created by Trinity College Dublin and local business with the objective to minimize 
+              youth unemployment.
             </p>
           </div>
         </div>
--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Card, CardActions, CardContent, Button } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import './Services.scss';
 
 function Services() {
+  const [course1, setCourse1] = useState(true);
+  const [course2, setCourse2] = useState(true);
+  const [course3, setCourse3] = useState(true);
+  const [course4, setCourse4] = useState(true);
+  const [course5, setCourse5] = useState(true);
+  const [course6, setCourse6] = useState(true);
+
   return (
     <div 
       id="services" 
@@ -16,13 +24,58 @@ function Services() {
       <section className="services-cards line-bottom">
         <Card className="services-card">
           <CardContent>
-            <h3 className="txt-border">HACCP - Level 1</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
-              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
-              nisi consectetur impedit, ut ullam nesciunt.
-            </p>
+            <h3 className="card-title txt-border">Food Safety - HACCP(Level 1)</h3>
+
+            <div className="card-description">
+              <div>
+                <p>
+                  The “Food Safety - HACCP  (Level 1)” course is being requested by Irish Government for those who want 
+                  to work in the food sector.
+                </p>
+                <p>
+                  The main advantage from other courses is that it was developed with an academic and entrepreneurial 
+                  vision, by a professional who has been working and studying this market for almost a decade and, 
+                  therefore, understands the requests of both owners and employees.
+                </p>
+
+                <div className={`card-link ${course1 ? '' : 'hide'}`}>
+                  <a 
+                    onClick={() => setCourse1(!course1)}
+                  >
+                    See more
+                  </a>
+                </div>
+              </div>
+              <div className={`${course1 ? 'hide' : ''}`}>
+                <p>
+                  Bartira had developed a practical content, solving the requirements from “Food Safety Authority of 
+                  Ireland  (FSAI)” and following all the principes from HACCP System (Hazard Analysis & Critical Control 
+                  Point).
+                </p>
+                <p>
+                  At the moment, the online course is only available in Portuguese Language. A face to face course is 
+                  available both in English or Portuguese.
+                </p>
+                <p>
+                  At the end of this course, we will provide a Digital Certificate in "Food Safety -  HACCP - Level 1”, 
+                  valid for 3 years in Ireland. In addition, the students will receive an e-book containing a course 
+                  summary and tips about the irish food market that may contribute to their career journey.
+                </p>
+                <div className="card-link">
+                    <a 
+                      onClick={() => setCourse1(!course1)}
+                    >
+                      <ArrowBackIcon />
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="card-price">
+              €40
+            </div>
           </CardContent>
+          
           <CardActions className="btn-container">
             <Button className="btn-orange">ADD TO CART</Button>
           </CardActions>
@@ -31,15 +84,72 @@ function Services() {
 
       <h2>Face-to-face Courses</h2>
       <section className="services-cards">
-        <Card className="services-card">
+
+      <Card className="services-card">
           <CardContent>
-            <h3 className="txt-border">HACCP - Level 1</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
-              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
-              nisi consectetur impedit, ut ullam nesciunt.
-            </p>
+            <h3 className="card title txt-border">Barista Course by Bartira</h3>
+
+            <div className="card-description">
+              <div>
+                <p>
+                  In 2013, when Bartira was a manager in a coffee shop, she started teaching friends and 
+                  customers who had requested for volunteer work as baristas. Since then, she has already 
+                  qualified more than 5000 baristas in all Ireland. 
+                </p>
+                <p>
+                  With a strong reputation in the market, the Barista Course by Bartira has offered the 
+                  right tools to impact the lives of many students who have been able to improve their 
+                  life standards after getting their first job as a barista.
+                </p>
+
+                <div className={`card-link ${course2 ? '' : 'hide'}`}>
+                  <a 
+                    onClick={() => setCourse2(!course2)}
+                  >
+                    See more
+                  </a>
+                </div>
+              </div>
+              <div className={`${course2 ? 'hide' : ''}`}>
+                <p>
+                  In this face to face course with 4h30, the student will learn about the history 
+                  of coffee, and develop all skills to know, by practicing, how to prepare the most 
+                  requested drinks such as: 
+                </p>
+                <p>
+                  Cappuccino, Macchiato, Americano, Latte, Hot Chocolate, Mocha, Espresso, Flat White and
+                  Ristretto.
+                </p>
+                <p>
+                  Furthermore, in her stylish coffee shop located in Dublin city center, Bartira will t
+                  each about the coffee machine, coffee art techniques, milk texture, coffee grounds and 
+                  all the necessary skills to become a barista.
+                </p>
+                <p>
+                  At the end of this course, the student will receive a Digital Certificate as "Barista”. 
+                  In addition, we will provide an e-book containing a course summary and tips about coffee 
+                  shops around Ireland that may contribute to their career journey.
+                </p>
+                <p>
+                  The materials for the course are included and the students could make and taste all 
+                  their own drinks. The Barista Course follows The Speciality Coffee Association of 
+                  Europe (SCAE) principles, where Bartira got her professional certification. 
+                </p>
+                <div className="card-link">
+                    <a 
+                      onClick={() => setCourse2(!course2)}
+                    >
+                      <ArrowBackIcon />
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="card-price">
+              €40
+            </div>
           </CardContent>
+          
           <CardActions className="btn-container">
             <Button className="btn-orange">ADD TO CART</Button>
           </CardActions>
@@ -47,13 +157,50 @@ function Services() {
 
         <Card className="services-card">
           <CardContent>
-            <h3 className="txt-border">HACCP - Level 1</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
-              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
-              nisi consectetur impedit, ut ullam nesciunt.
-            </p>
+            <h3 className="txt-border">CVIEW</h3>
+
+            <div className="card-description">
+              <div>
+                <p>
+                  The CVIEW project was created after many requests, especially from students who wanted to 
+                  get a job but had no idea how to start or even create a Curriculum Vitae (CV). 
+                </p>
+                <p>
+                  This online service will think and design your C.V., through the support of an 
+                  Entrepreneur, who will have a chat to understand your characteristics, skills, goals, 
+                  professional and personal experiences and qualifications to help you in your journey to 
+                  get a job in Ireland.
+                </p>
+
+                {/* <div className={`card-link ${course3 ? '' : 'hide'}`}>
+                  <a 
+                    onClick={() => setCourse3(!course3)}
+                  >
+                    See more
+                  </a>
+                </div> */}
+              </div>
+              {/* <div className={`${course3 ? 'hide' : ''}`}>
+                <p>
+                  Bartira had developed a practical content, solving the requirements from “Food Safety Authority of 
+                  Ireland  (FSAI)” and following all the principes from HACCP System (Hazard Analysis & Critical Control 
+                  Point).
+                </p>
+                <div className="card-link">
+                    <a 
+                      onClick={() => setCourse3(!course3)}
+                    >
+                      <ArrowBackIcon />
+                    </a>
+                </div>
+              </div> */}
+            </div>
+
+            <div className="card-price">
+              €40
+            </div>
           </CardContent>
+          
           <CardActions className="btn-container">
             <Button className="btn-orange">ADD TO CART</Button>
           </CardActions>
@@ -61,13 +208,48 @@ function Services() {
 
         <Card className="services-card">
           <CardContent>
-            <h3 className="txt-border">HACCP - Level 1</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
-              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
-              nisi consectetur impedit, ut ullam nesciunt.
-            </p>
+            <h3 className="txt-border">Combo Start</h3>
+
+            <div className="card-description">
+              <div>
+                <p>
+                  Barista Course (face to face) + Food Safety (online) for €55.
+                </p>
+                <p>
+                  Getting this combo, you will receive an online Food Safety training and also a face to face Barista training. 
+                </p>
+
+                <div className={`card-link ${course4 ? '' : 'hide'}`}>
+                  <a 
+                    onClick={() => setCourse4(!course4)}
+                  >
+                    See more
+                  </a>
+                </div>
+              </div>
+              <div className={`${course4 ? 'hide' : ''}`}>
+                <p>
+                  With both certifications, you will be able to work in all the irish food sectors such as 
+                  coffee shops, bakeries, supermarkets, restaurants and much more. 
+                </p>
+                <p>
+                  We are looking forward to seeing you!
+                </p>
+                <div className="card-link">
+                    <a 
+                      onClick={() => setCourse4(!course4)}
+                    >
+                      <ArrowBackIcon />
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="card-price">
+              €55
+            </div>
           </CardContent>
+          
           <CardActions className="btn-container">
             <Button className="btn-orange">ADD TO CART</Button>
           </CardActions>
@@ -75,13 +257,47 @@ function Services() {
 
         <Card className="services-card">
           <CardContent>
-            <h3 className="txt-border">HACCP - Level 1</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
-              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
-              nisi consectetur impedit, ut ullam nesciunt.
-            </p>
+            <h3 className="txt-border">Combo Head Start</h3>
+
+            <div className="card-description">
+              <div>
+                <p>
+                  Barista Course (face to face) + Food Safety (online) + CVIEW for €75.
+                </p>
+                <p>
+                  This is a premium combo to prepare you with all the important skills to become a 
+                  successful professional in the food sector.
+                </p>
+
+                <div className={`card-link ${course5 ? '' : 'hide'}`}>
+                  <a 
+                    onClick={() => setCourse5(!course5)}
+                  >
+                    See more
+                  </a>
+                </div>
+              </div>
+              <div className={`${course5 ? 'hide' : ''}`}>
+                <p>
+                  With the Head Start Combo you will have a discount in our online Food Safety training, 
+                  a face to face Barista training and a Curriculum Vitae support to get the perfect head 
+                  start in your journey. Are you ready?
+                </p>
+                <div className="card-link">
+                    <a 
+                      onClick={() => setCourse5(!course5)}
+                    >
+                      <ArrowBackIcon />
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="card-price">
+              €75
+            </div>
           </CardContent>
+          
           <CardActions className="btn-container">
             <Button className="btn-orange">ADD TO CART</Button>
           </CardActions>
@@ -89,17 +305,51 @@ function Services() {
 
         <Card className="services-card">
           <CardContent>
-            <h3 className="txt-border">HACCP - Level 1</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor totam doloremque soluta 
-              quis obcaecati reprehenderit vero quia recusandae accusantium nam? Eius et blanditiis aliquam 
-              nisi consectetur impedit, ut ullam nesciunt.
-            </p>
+            <h3 className="txt-border">Combo Stars</h3>
+
+            <div className="card-description">
+              <div>
+                <p>
+                  From groups of 5 or more students, we are offering €5 discount each in any service.
+                </p>
+                <p>
+                  Join the courses with your friends and start growing together!
+                </p>
+
+                {/* <div className={`card-link ${course6 ? '' : 'hide'}`}>
+                  <a 
+                    onClick={() => setCourse6(!course6)}
+                  >
+                    See more
+                  </a>
+                </div> */}
+              </div>
+              {/* <div className={`${course6 ? 'hide' : ''}`}>
+                <p>
+                  Bartira had developed a practical content, solving the requirements from “Food Safety Authority of 
+                  Ireland  (FSAI)” and following all the principes from HACCP System (Hazard Analysis & Critical Control 
+                  Point).
+                </p>
+                <div className="card-link">
+                    <a 
+                      onClick={() => setCourse6(!course6)}
+                    >
+                      <ArrowBackIcon />
+                    </a>
+                </div>
+              </div> */}
+            </div>
+
+            <div className="card-price">
+              €40
+            </div>
           </CardContent>
+          
           <CardActions className="btn-container">
             <Button className="btn-orange">ADD TO CART</Button>
           </CardActions>
         </Card>
+
       </section>
     </div>
   );
