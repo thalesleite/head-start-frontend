3b7645dabe4985458b23927ace4d203fee5b4704
thalesleite <thalesaleite@gmail.com>
2020-09-29 21:59:36 +0100

    path changed

--- src/App.js
+++ src/App.js
@@ -65,10 +65,10 @@ class App extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route path='/course/:path?' exact>
+          <Route>
             <MainCourse>
               <Switch>
-                <Route exact path='/course' component={CoursePage} />
+                <Route path='/course' component={CoursePage} />
               </Switch>
             </MainCourse>
           </Route>
