e5eb6993ce20eb61829e6be7288616464d75e947
thalesleite <thalesaleite@gmail.com>
2020-10-21 10:15:08 +0100

    changed domain name server

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
 const dev = 'http://localhost:3333';
-const qa = 'https://head-start-app.herokuapp.com/';
-const prod = 'https://headstartcourses.ie/';
+const prod = 'https://head-start-app.herokuapp.com/';
 
 const api = axios.create({
     baseURL: prod,
-    //baseURL: qa,
     //baseURL: dev,
 });
 
